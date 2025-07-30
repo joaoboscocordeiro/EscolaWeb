f0b4c441bae2f7a508190dcb8f15e2e2851397b9
João Bosco Cordeiro de Lima Júnior <ti.junior@gmail.com>
2025-07-30 20:07:44 -0300

    Criando layout cadastrar aluno

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -1,15 +1,20 @@
-﻿using EscolaWeb.Services.Aluno;
+﻿using EscolaWeb.Models;
+using EscolaWeb.Services.Aluno;
+using EscolaWeb.Services.Turma;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EscolaWeb.Controllers
 {
     public class AlunoController : Controller
     {
         private readonly IAlunoInterface _alunoInterface;
+        private readonly ITurmaInterface _turmaInterface;
 
-        public AlunoController(IAlunoInterface alunoInterface)
+        public AlunoController(IAlunoInterface alunoInterface, ITurmaInterface turmaInterface)
         {
             _alunoInterface = alunoInterface;
+            _turmaInterface = turmaInterface;
         }
 
         [HttpGet]
@@ -20,11 +25,32 @@ namespace EscolaWeb.Controllers
         }
 
         [HttpGet]
+        public IActionResult CadastrarAluno()
+        {
+            BuscarTurmas();
+            return View();
+        }
+
+        [HttpGet]
         [Route("/Aluno/AlunosDaTurma/{idTurma}")]
         public IActionResult AlunosDaTurma(int idTurma)
         {
             var alunos = _alunoInterface.BuscarAlunosPorTurma(idTurma);
             return Json(new {dados = alunos});
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarAluno(AlunoModel alunoModel)
+        {
+            return View();
+        }
+
+        private void BuscarTurmas()
+        {
+            var turmas = _turmaInterface.BuscarTurmas();
+            var listaTurma = new SelectList(turmas, "Id", "Descricao");
+
+            ViewBag.Turmas = listaTurma;
         }
     }
 }
